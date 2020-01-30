1fee05127c5cff2436c2689b60fd56dcd9152c83
Steven Jackson <stevenpjackson0@gmail.com>
2020-01-30 13:45:36 -0600

    Modify (data.php>Hunk 1) : Change to use an OOP approach to the same small script and exception handling.

--- data.php
+++ data.php
@@ -2,15 +2,20 @@
 //Display only fatal errors
 error_reporting(1);
 
-//Open text file and add contents to $file
-//Creates a warning if there is no file and dispaly_errors on  
-try{
-if(!$file = fopen("data.txt", "r")){
-    //Display errors is turned off so we dont see the error below 
-    //We stil recieve a fatal error that stops the script
-    throw new Exception('Unable to access file');
+class myData
+{
+        function getData()
+        {
+            if(!$file = fopen("data.txt", "r")){
+                throw new Exception('Unable to access file');
+        }
     }
-    //Exectpion handling shows error and allows script to continue
+}
+
+$data = new myData();
+
+try{
+    $data->getData();
 } catch (Exception $e){
     echo $e->getMessage();
 }

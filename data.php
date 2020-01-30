41f0ea83cabcd7ad0dc8c40692588934da5a5b84
Steven Jackson <stevenpjackson0@gmail.com>
2020-01-30 13:27:37 -0600

    Modify (data.php>Hunk 1) : Set error_reporting to 1 and create try catch block to procedurally handle the any fatal errors.

--- data.php
+++ data.php
@@ -1,10 +1,19 @@
 <?php
-//Turn off error display 
-ini_set('display_errors', 'Off');
+//Display only fatal errors
+error_reporting(1);
 
 //Open text file and add contents to $file
 //Creates a warning if there is no file and dispaly_errors on  
-$file = fopen("data.txt", "r");
+try{
+if(!$file = fopen("data.txt", "r")){
+    //Display errors is turned off so we dont see the error below 
+    //We stil recieve a fatal error that stops the script
+    throw new Exception('Unable to access file');
+    }
+    //Exectpion handling shows error and allows script to continue
+} catch (Exception $e){
+    echo $e->getMessage();
+}
 
 //Echo statement still runs since script do not stop during warnings 
 echo "End of FIle";

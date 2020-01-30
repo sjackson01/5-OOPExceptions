c1e7d6e8c1585b8f1d6e2e95140e951f2ddd5335
Steven Jackson <stevenpjackson0@gmail.com>
2020-01-30 12:23:19 -0600

    Add (index.php>Hunk 1) : Create example try catch block with exception $e and associated methods.

--- index.php
+++ index.php
@@ -2,3 +2,10 @@
 
 /* PHP OOP Exceptions */ 
 
+try {
+    $db = new PDO("sqlite:".__DIR__."/database.db");
+}catch(Exceptions $e){
+    echo "Unable to Connect: " . $e->getMessage()
+        ." in File: " . $e->getFile()
+        ." on line " . $e->getLine();
+}
